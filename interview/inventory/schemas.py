6e22959183a84c60612e57339457bdc3dd8e3cec
b123m <brian.pitts@tmtinsights.com>
2023-07-21 17:48:09 -0400

    changes for short test

--- interview/inventory/schemas.py
+++ interview/inventory/schemas.py
@@ -1,10 +1,11 @@
 
 from decimal import Decimal
+from typing import List
 from pydantic import BaseModel
 
 
 class InventoryMetaData(BaseModel):
     year: int
-    actors: list[str]
+    actors: List[str]
     imdb_rating: Decimal
     rotten_tomatoes_rating: int
