c49cd9ebb3ff0014825a625f0ac9dc5bf3e2ad60
Sebastian Kalinowski <sebastian@kalinowski.eu>
2023-07-28 11:09:18 +0200

    chore: add linting config & lint all the things

--- interview/inventory/serializers.py
+++ interview/inventory/serializers.py
@@ -4,24 +4,21 @@ from interview.inventory.models import Inventory, InventoryLanguage, InventoryTa
 
 
 class InventoryTagSerializer(serializers.ModelSerializer):
-    
     class Meta:
         model = InventoryTag
-        fields = ['id', 'name', 'is_active']
-        
-        
+        fields = ["id", "name", "is_active"]
+
+
 class InventoryLanguageSerializer(serializers.ModelSerializer):
-    
     class Meta:
         model = InventoryLanguage
-        fields = ['id', 'name']
+        fields = ["id", "name"]
 
 
 class InventoryTypeSerializer(serializers.ModelSerializer):
-    
     class Meta:
         model = InventoryType
-        fields = ['id', 'name']
+        fields = ["id", "name"]
 
 
 class InventorySerializer(serializers.ModelSerializer):
@@ -29,7 +26,7 @@ class InventorySerializer(serializers.ModelSerializer):
     language = InventoryLanguageSerializer()
     tags = InventoryTagSerializer(many=True)
     metadata = serializers.JSONField()
-    
+
     class Meta:
         model = Inventory
-        fields = ['id', 'name', 'type', 'language', 'tags', 'metadata']
+        fields = ["id", "name", "type", "language", "tags", "metadata"]
