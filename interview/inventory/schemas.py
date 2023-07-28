fe76b52ce72e4557e011c9a8ee103cc8aecd8f1d
Sebastian Kalinowski <sebastian@kalinowski.eu>
2023-07-28 11:09:18 +0200

    chore: add linting config & lint all the things

--- interview/inventory/schemas.py
+++ interview/inventory/schemas.py
@@ -1,11 +1,10 @@
-
 from decimal import Decimal
-from typing import List
+
 from pydantic import BaseModel
 
 
 class InventoryMetaData(BaseModel):
     year: int
-    actors: List[str]
+    actors: list[str]
     imdb_rating: Decimal
     rotten_tomatoes_rating: int
